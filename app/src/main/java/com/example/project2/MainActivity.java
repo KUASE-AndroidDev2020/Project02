9621f8b0a478bd3b10c29df11f848f867110b208
KOTARO-ITATSU <2020m098@kuas.ac.jp>
2020-06-29 12:22:17 +0900

    I made  rules, results and counts.

--- app/src/main/java/com/example/project2/MainActivity.java
+++ app/src/main/java/com/example/project2/MainActivity.java
@@ -12,7 +12,12 @@ import java.util.Random;
 public class MainActivity extends AppCompatActivity {
     TextView player;
     TextView cpu;
+    TextView result;
+    TextView player2;
+    TextView cpu2;
     int number;
+    int playerpoint;
+    int cpupoint;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +25,11 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         player = (TextView)findViewById(R.id.player);
         cpu = (TextView)findViewById(R.id.cpu);
+        result = (TextView)findViewById(R.id.result);
+        player2 = (TextView)findViewById(R.id.player2);
+        cpu2 = (TextView)findViewById(R.id.cpu2);
+        playerpoint = 0;
+        cpupoint = 0;
     }
 
     public void goo(View view) {
@@ -30,14 +40,21 @@ public class MainActivity extends AppCompatActivity {
         if(number==0){
 
             cpu.setText("The enemy's hand is ROCK.");
+            result.setText("You are draw.");
 
         }else if(number==1){
 
-            cpu.setText("The enemy's hand is SCISSOR");
+            cpu.setText("The enemy's hand is SCISSOR.");
+            result.setText("You win.");
+            playerpoint = playerpoint + 1;
+            player2.setText("Your point is"+playerpoint);
 
         }else if(number==2){
 
-            cpu.setText("The enemy's hand is PAPER");
+            cpu.setText("The enemy's hand is PAPER.");
+            result.setText("you defeat.");
+            cpupoint = cpupoint + 1;
+            cpu2.setText("The enemy's point is"+cpupoint);
 
         }
     }
@@ -50,34 +67,48 @@ public class MainActivity extends AppCompatActivity {
         if(number==0){
 
             cpu.setText("The enemy's hand is ROCK.");
+            result.setText("you defeat.");
+            cpupoint = cpupoint + 1;
+            cpu2.setText("The enemy's point is"+cpupoint);
 
         }else if(number==1){
 
-            cpu.setText("The enemy's hand is SCISSOR");
+            cpu.setText("The enemy's hand is SCISSOR.");
+            result.setText("You are draw.");
 
         }else if(number==2){
 
-            cpu.setText("The enemy's hand is PAPER");
+            cpu.setText("The enemy's hand is PAPER.");
+            result.setText("You win.");
+            playerpoint = playerpoint + 1;
+            player2.setText("Your point is"+playerpoint);
 
         }
     }
 
     public void paa(View view) {
-        player.setText("Your hand is PAPER");
+        player.setText("Your hand is PAPER.");
         Random random = new Random();
         number = random.nextInt(3);
 
         if(number==0){
 
             cpu.setText("The enemy's hand is ROCK.");
+            result.setText("You win.");
+            playerpoint = playerpoint + 1;
+            player2.setText("Your point is"+playerpoint);
 
         }else if(number==1){
 
-            cpu.setText("The enemy's hand is SCISSOR");
+            cpu.setText("The enemy's hand is SCISSOR.");
+            result.setText("you defeat.");
+            cpupoint = cpupoint + 1;
+            cpu2.setText("The enemy's point is"+cpupoint);
 
         }else if(number==2){
 
-            cpu.setText("The enemy's hand is PAPER");
+            cpu.setText("The enemy's hand is PAPER.");
+            result.setText("You are draw.");
 
         }
     }
